815b874de4e214a071e50c76359dda7b461309ad
GIT_AUTHOR_NAME <GIT_AUTHOR_EMAIL>
2023-03-07 21:35:29 +1100

    before configuring the SQL server management studio

--- Models/ApplicationDbContext.cs
+++ Models/ApplicationDbContext.cs
@@ -4,12 +4,12 @@ namespace budget_monitor_app.Models
 {
     public class ApplicationDbContext:DbContext
     {
-        public ApplicationDbContext()
-        {}
+        public ApplicationDbContext(DbContextOptions options): base(options) { }
 
         public DbSet<Transaction> Transactions { get; set; }
-        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Category> Categories { get; set; }
 
 
     }
 }
+ 
