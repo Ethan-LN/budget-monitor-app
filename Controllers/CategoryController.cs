2d4bc2270a97ded8546e596e90cf42e07e057896
GIT_AUTHOR_NAME <GIT_AUTHOR_EMAIL>
2023-03-10 22:20:29 +1100

    configure the sql server and make the Category controller

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using budget_monitor_app.Models;
+
+namespace budget_monitor_app.Controllers
+{
+    public class CategoryController : Controller
+
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Category
+        public async Task<IActionResult> Index() {
+
+            return _context.Categories != null ?
+                View(await _context.Categories.ToListAsync()) :
+                Problem("Entity set 'ApplicationDcContext.Categories' is null.");
+        }
+
+        public IActionResult AddOrEdit(int id = 0) {
+            if (id == 0)
+            {
+                return View(new Category());
+            }
+            else 
+            {
+                return View(_context.Categories.Find(id));
+            }
+        }
+
+        //POST: Category/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id) 
+        {
+            if (_context.Categories == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Categories' is null.");
+            }
+            var category = await _context.Categories.FindAsync(id);
+            if (category != null)
+            {
+                _context.Categories.Remove(category);
+            }
+            await _context.SaveChangesAsync();
+            return View(new Category());
+        }
+    }
+}
